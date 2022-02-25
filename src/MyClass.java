c932a0b46ef193a24c1ce764e77b1b8648fcdfcf
jessi1203 <jessii.ca@live.dk>
2022-02-25 10:24:49 +0100

    del 1 commit

--- src/MyClass.java
+++ src/MyClass.java
@@ -1,2 +1,11 @@
 public class MyClass {
+
+    void stupidMethod() {
+        System.out.println("This is stupid");
+    }
+
+
+    public static void main(String[] args) {
+        new MyClass().stupidMethod();
+    }
 }
