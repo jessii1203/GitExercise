d2854117327c8dc6ba9ac565d49d57ca107fd477
jessi1203 <jessii.ca@live.dk>
2022-02-25 10:40:12 +0100

    Shouldn't  have done that 2

--- src/MyClass.java
+++ src/MyClass.java
@@ -1,5 +1,8 @@
 public class MyClass {
 
+    void go(){
+        System.out.println("nu prøver vi noget nyt");
+    }
     void stupidMethod() {
         System.out.println("This is stupid");
     }
